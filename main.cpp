589317b7ff14f06ecceb02920ff8f9d92e0a03ab
marybadalyan <marybadalyangdh@gmail.com>
2025-03-01 19:01:45 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ std::atomic<bool> running(true);
 const size_t BUFFER_SIZE = 1ULL * 1024ULL * 1024ULL * 1024ULL; // 1 GB
 const size_t ITERATIONS = 20;
 
-const size_t THREAD_COUNT = std::thread::hardware_concurrency();
+#ifdef _WIN32
+const size_t THREAD_COUNT = 10; // Windows prefers higher threads
+#else
+const size_t THREAD_COUNT = std::thread::hardware_concurrency();  // Ubuntu prefers hardware concurrency
+#endif
 
 // Custom allocator for 32-byte alignment
 template<typename T>
